682fbcd1cfe4a5967273c0173c211d353f9bc5fa
Eric Anderson <elanderson@gmail.com>
2016-08-12 08:10:59 -0500

    Add contacts api controller

---
+++ ASP.NET%20Core%20Basics/src/Contacts/Controllers/ContactsApiController.cs
@@ -0,0 +1,139 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Contacts.Models;
+
+namespace Contacts.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/ContactsApi")]
+    public class ContactsApiController : Controller
+    {
+        private readonly ContactsContext _context;
+
+        public ContactsApiController(ContactsContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/ContactsApi
+        [HttpGet]
+        public IEnumerable<Contact> GetContact()
+        {
+            return _context.Contact;
+        }
+
+        // GET: api/ContactsApi/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetContact([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            Contact contact = await _context.Contact.SingleOrDefaultAsync(m => m.Id == id);
+
+            if (contact == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(contact);
+        }
+
+        // PUT: api/ContactsApi/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutContact([FromRoute] int id, [FromBody] Contact contact)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != contact.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(contact).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ContactExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/ContactsApi
+        [HttpPost]
+        public async Task<IActionResult> PostContact([FromBody] Contact contact)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.Contact.Add(contact);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (ContactExists(contact.Id))
+                {
+                    return new StatusCodeResult(StatusCodes.Status409Conflict);
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetContact", new { id = contact.Id }, contact);
+        }
+
+        // DELETE: api/ContactsApi/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteContact([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            Contact contact = await _context.Contact.SingleOrDefaultAsync(m => m.Id == id);
+            if (contact == null)
+            {
+                return NotFound();
+            }
+
+            _context.Contact.Remove(contact);
+            await _context.SaveChangesAsync();
+
+            return Ok(contact);
+        }
+
+        private bool ContactExists(int id)
+        {
+            return _context.Contact.Any(e => e.Id == id);
+        }
+    }
+}
