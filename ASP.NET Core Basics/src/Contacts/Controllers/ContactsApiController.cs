b67097b72a8e94ef5815bda27958d36aac8ceafe
Eric Anderson <efcela@outlook.com>
2016-08-18 06:38:14 -0500

    Move contacts API controller to the proper folder.

--- ASP.NET Core Basics/src/Contacts/Controllers/ContactsApiController.cs
+++ ASP.NET Core Basics/src/Contacts/Controllers/ContactsApiController.cs
@@ -1,7 +1,7 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Contacts.Data;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
