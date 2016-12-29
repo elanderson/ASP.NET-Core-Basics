34c20d38d5589a1b98510def00a04292e578c582
Eric Anderson <elanderson@gmail.com>
2016-12-29 06:10:32 -0600

    Create contact detail in Angular. (#5)
    * Add files and part of the implementation for contact detail in the Angular project.
    
    * Complete contact detail changes for Angular project.

--- ASP.NET Core Basics/src/Angular/ClientApp/app/components/contacts/contact.service.ts
+++ ASP.NET Core Basics/src/Angular/ClientApp/app/components/contacts/contact.service.ts
@@ -5,15 +5,24 @@ import { Contact } from './contact';
 
 @Injectable()
 export class ContactService {
+    baseUrl = 'http://localhost:13322/api/contactsApi/';
 
     constructor(private http: Http) {
     } 
 
     getAll(): Promise<Contact[]> {
-        return this.http.get('http://localhost:13322/api/contactsApi/')
+        return this.http.get(this.baseUrl)
             .toPromise()
             .then(response => response.json())
             .then(contacts => Array.from(contacts, c => new Contact(c)))
+            .catch(error => console.log(error));
+    }
+
+    getById(id: string): Promise<Contact> {
+        return this.http.get(this.baseUrl + id)
+            .toPromise()
+            .then(response => response.json())
+            .then(contact => new Contact(contact))
             .catch(error => console.log(error));
     }
 }
---
+++ ASP.NET Core Basics/src/Angular/ClientApp/app/components/contacts/contactdetail.component.ts
@@ -0,0 +1,24 @@
+﻿import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import 'rxjs/add/operator/switchMap';
+import { Contact } from './contact';
+import { ContactService } from './contact.service';
+
+@Component({
+    selector: 'contactdetail',
+    template: require('./contactdetail.component.html'),
+    providers: [ContactService]
+})
+export class ContactDetailComponent implements OnInit {
+    contact: Contact;
+
+    constructor(private route: ActivatedRoute,
+                private router: Router,
+                private contactService: ContactService) { }
+
+    ngOnInit(): void {
+        this.route.params
+            .switchMap((params: Params) => this.contactService.getById(params['id']))
+            .subscribe((contact :Contact) => this.contact = contact);
+    }
+}
