029e2ed4a2e1f1476722a50e99b2b157d8aad2d4
Eric Anderson <elanderson@gmail.com>
2017-10-11 06:39:10 -0500

    Add contact list

--- ASP.NET Core Basics/React/ClientApp/components/NavMenu.tsx
+++ ASP.NET Core Basics/React/ClientApp/components/NavMenu.tsx
@@ -32,6 +32,11 @@ export class NavMenu extends React.Component<{}, {}> {
                                 <span className='glyphicon glyphicon-th-list'></span> Fetch data
                             </NavLink>
                         </li>
+                        <li>
+                            <NavLink to={'/contactlist'} activeClassName='active'>
+                                <span className='glyphicon glyphicon-th-list-alt'></span> Contact List
+                            </NavLink>
+                        </li>
                     </ul>
                 </div>
             </div>
--- ASP.NET Core Basics/React/ClientApp/routes.tsx
+++ ASP.NET Core Basics/React/ClientApp/routes.tsx
@@ -4,9 +4,11 @@ import { Layout } from './components/Layout';
 import { Home } from './components/Home';
 import { FetchData } from './components/FetchData';
 import { Counter } from './components/Counter';
+import { ContactList } from './components/ContactList';
 
 export const routes = <Layout>
-    <Route exact path='/' component={ Home } />
-    <Route path='/counter' component={ Counter } />
-    <Route path='/fetchdata' component={ FetchData } />
+    <Route exact path='/' component={Home} />
+    <Route path='/counter' component={Counter} />
+    <Route path='/fetchdata' component={FetchData} />
+    <Route path='/contactlist' component={ContactList} />
 </Layout>;
