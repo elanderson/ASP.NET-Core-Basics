564da6f1b61c06da169a67adf69fdc7ef971ef95
Eric Anderson <elanderson@gmail.com>
2017-10-14 13:32:15 -0500

    Add React project with basic contact list (#19)

---
+++ ASP.NET Core Basics/React/ClientApp/components/ContactList.tsx
@@ -0,0 +1,62 @@
+﻿import * as React from 'react';
+import { RouteComponentProps } from 'react-router';
+import 'isomorphic-fetch';
+
+interface ContactListState {
+    contacts: Contact[];
+    loading: boolean;
+}
+
+export class ContactList extends React.Component<RouteComponentProps<{}>, ContactListState> {
+    constructor() {
+        super();
+        this.state = { contacts: [], loading: true };
+
+        fetch('http://localhost:13322/api/contactsApi/')
+            .then(response => response.json() as Promise<Contact[]>)
+            .then(data => {
+                this.setState({ contacts: data, loading: false });
+            });
+    }
+
+    public render() {
+        let contents = this.state.loading
+            ? <p><em>Loading...</em></p>
+            : ContactList.renderContactsTable(this.state.contacts);
+
+        return <div>
+            <h1>Contact List</h1>
+            {contents}
+        </div>;
+    }
+
+    private static renderContactsTable(contacts: Contact[]) {
+        return <table className='table'>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                </tr>
+            </thead>
+            <tbody>
+                {contacts.map(contact =>
+                    <tr key={contact.id}>
+                        <td>{contact.id}</td>
+                        <td>{contact.name}</td>
+                    </tr>
+                )}
+            </tbody>
+        </table>;
+    }
+}
+
+interface Contact {
+    id: number;
+    name: string;
+    address: string;
+    city: string;
+    state: string;
+    postalCode: string;
+    phone: string;
+    email: string;
+}
---
+++ ASP.NET Core Basics/React/ClientApp/routes.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import { Route } from 'react-router-dom';
+import { Layout } from './components/Layout';
+import { Home } from './components/Home';
+import { FetchData } from './components/FetchData';
+import { Counter } from './components/Counter';
+import { ContactList } from './components/ContactList';
+
+export const routes = <Layout>
+    <Route exact path='/' component={Home} />
+    <Route path='/counter' component={Counter} />
+    <Route path='/fetchdata' component={FetchData} />
+    <Route path='/contactlist' component={ContactList} />
+</Layout>;
