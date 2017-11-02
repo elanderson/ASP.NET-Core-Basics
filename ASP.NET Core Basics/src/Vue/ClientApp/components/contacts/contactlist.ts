2df21bdbe997e6ee142a9683fb062f07340b025b
Eric Anderson <elanderson@gmail.com>
2017-11-02 06:27:39 -0500

    Vue - Add contact list (#22)
    * Add Vue project
    
    * Add contact list to Vue application

---
+++ ASP.NET Core Basics/src/Vue/ClientApp/components/contacts/contactlist.ts
@@ -0,0 +1,26 @@
+﻿import Vue from 'vue';
+import { Component } from 'vue-property-decorator';
+
+@Component
+export default class ContactListComponent extends Vue {
+    contacts: Contact[] = [];
+
+    mounted() {
+        fetch('http://localhost:13322/api/contactsApi/')
+            .then(response => response.json() as Promise<Contact[]>)
+            .then(data => {
+                this.contacts = data;
+            });
+    }
+}
+
+interface Contact {
+    id: number;
+    name: string;
+    address: string;
+    city: string;
+    state: string;
+    postalCode: string;
+    phone: string;
+    email: string;
+}
