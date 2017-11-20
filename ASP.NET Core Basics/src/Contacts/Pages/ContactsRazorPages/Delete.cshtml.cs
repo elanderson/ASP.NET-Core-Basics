ba39c7b3ee59ebc02be81790189171e80042fcb3
Eric Anderson <elanderson@gmail.com>
2017-11-20 06:34:57 -0600

    Add Razor Pages example of contact list (#24)

---
+++ ASP.NET Core Basics/src/Contacts/Pages/ContactsRazorPages/Delete.cshtml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Contacts.Data;
+using Contacts.Models;
+
+namespace Contacts.Pages.ContactsRazorPages
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly Contacts.Data.ContactsContext _context;
+
+        public DeleteModel(Contacts.Data.ContactsContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Contact Contact { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Contact = await _context.Contact.SingleOrDefaultAsync(m => m.Id == id);
+
+            if (Contact == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Contact = await _context.Contact.FindAsync(id);
+
+            if (Contact != null)
+            {
+                _context.Contact.Remove(Contact);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
