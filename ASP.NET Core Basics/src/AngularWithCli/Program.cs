c60101d00cb876f22088b745dcc90a12c3cdacd4
Eric Anderson <eanderson@efcsystems.com>
2018-06-05 07:00:01 -0500

    Upgrade rest of projects. Not tested.

--- ASP.NET Core Basics/src/AngularWithCli/Program.cs
+++ ASP.NET Core Basics/src/AngularWithCli/Program.cs
@@ -16,7 +16,7 @@ namespace AngularWithCli
         {
             CreateWebHostBuilder(args).Build().Run();
         }
-
+ 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>();
